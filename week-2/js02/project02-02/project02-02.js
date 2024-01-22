ed4723d8cde7114ef713c6e4d0fc6a1b8f373559
Charlie Brandenburg <148399772+charliebrandenburg@users.noreply.github.com>
2024-01-21 20:19:54 -0600

    Add files via upload

---
+++ week-2/js02/project02-02/project02-02.js
@@ -0,0 +1,23 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Charlie Brandenburg
+      Date: 01/21/24
+
+      Filename: project02-02.js
+ */
+
+      // form setup
+      function verifyForm() {
+      <input type "button" ; onclick = "verifyForm">
+
+    // handle the form data
+
+        let Name = document.getElementById("nameinput");
+        let Email = document.getElementById("emailinput");
+        let Phone = document.getElementById("phoneinput");
+
+        let contactinfo = (nameinput === "Name" && emailinput ==="Email" && phoneinput === "Phone") ? true : false;
+        }
