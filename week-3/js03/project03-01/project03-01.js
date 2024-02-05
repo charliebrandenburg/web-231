75a3982f7144fda99a2cd84ca15cfd0147469c08
Charlie Brandenburg <148399772+charliebrandenburg@users.noreply.github.com>
2024-02-04 22:12:50 -0600

    Add files via upload

---
+++ week-3/js03/project03-01/project03-01.js
@@ -0,0 +1,41 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Charlie Brandenburg
+      Date: 02/04/24
+
+      Filename: project03-01.js
+*/
+// declare menuItems
+let menuItems = document.getElementsByClassName();
+// create for loop
+function menuItems() {
+  for (let i = 0; i < menuItems.length; i++) {
+    let menuItems = "";
+    // event listener
+    document.getElementsByClassName("menuItems[i]").addEventListener("click", calcTotal, true);
+  }
+}
+
+let orderTotal = (0); {
+let item1 = document.getElementById("item1").value;
+let item2 = document.getElementById("item2").value;
+let item3 = document.getElementById("item3").value;
+let item4 = document.getElementById("item4").value;
+let item5 = document.getElementById("item5").value;
+
+if (item1[i] < 5) {
+} else if (item2[i] < 5) {
+} else if (item3[i] > 5) {
+} else if (item4[i] < 5) {
+} else if (item5[i] > 5) {
+}
+document.getElementById("menuItems").innerHTML = "$" + totalCost;
+}
+
+ // Function to display a numeric value as a text string in the format $##.##
+function formatCurrency() {
+
+}
